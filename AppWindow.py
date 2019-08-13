498ec24dab3c899492ab1622a55f4ea2b36b4865
denzel-bogues <53503061+denzel-bogues@users.noreply.github.com>
2019-08-13 19:48:27 -0400

    Add files via upload

--- AppWindow.py
+++ AppWindow.py
@@ -1,42 +1,48 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys
+from PyQt5.QtGui import QIntValidator, QFont
 import pandas as p
-from pandas.api.types import is_string_dtype
-from pandas.api.types import is_numeric_dtype
-
-red = 0
-green = 0
-blue = 0
+import sys
 
 
-class Ui_MainWindow(object):
+class UiMainWindow(object):
     def __init__(self, MainWindow):
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.verticalLayout_OutputWidget = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+
         self.ConfirmButton = QtWidgets.QPushButton(self.centralwidget)
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.bluevalue_input = QtWidgets.QLineEdit(self.verticalLayoutWidget)
-        self.greenvalue_input = QtWidgets.QLineEdit(self.verticalLayoutWidget)
-        self.redvalue_input = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+
+        self.verticalLayout_Output = QtWidgets.QVBoxLayout(self.verticalLayout_OutputWidget)
+
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
         self.BlueValue = QtWidgets.QLabel(self.verticalLayoutWidget_2)
         self.GreenValue = QtWidgets.QLabel(self.verticalLayoutWidget_2)
         self.RedValue = QtWidgets.QLabel(self.verticalLayoutWidget_2)
 
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.bluevalue_input = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.greenvalue_input = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.redvalue_input = QtWidgets.QLineEdit(self.verticalLayoutWidget)
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 200)
-        MainWindow.setMinimumSize(QtCore.QSize(400, 200))
-        MainWindow.setAutoFillBackground(False)
+        MainWindow.resize(400, 150)
+        MainWindow.setMinimumSize(QtCore.QSize(400, 150))
+        MainWindow.setMaximumSize(400, 200)
+        MainWindow.setAutoFillBackground(True)
         MainWindow.setWindowIcon(QtGui.QIcon("homeIcon.jpg"))
         self.__init__(MainWindow)
 
     def input_widgets(self, MainWindow):
         self.centralwidget.setObjectName("centralwidget")
+
+        self.verticalLayout_OutputWidget.setGeometry(QtCore.QRect(250, 10, 100, 125))
+        self.verticalLayout_OutputWidget.setObjectName("verticalLayout_OutputWidget")
+        self.verticalLayout_Output.setContentsMargins(0, 0, 0, 00)
+        self.verticalLayout_Output.setObjectName("verticalLayout_Output")
 
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(150, 0, 91, 80))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
@@ -45,18 +51,15 @@ class Ui_MainWindow(object):
 
         self.redvalue_input.setObjectName("redvalue_input")
         self.verticalLayout.addWidget(self.redvalue_input)
-      #  if self.redvalue_input.isModified() == False:
-        #    self.redvalue_input.setText("-1")
+        self.redvalue_input.setValidator(QIntValidator(0, 255))
 
         self.greenvalue_input.setObjectName("greenvalue_input")
         self.verticalLayout.addWidget(self.greenvalue_input)
-      #  if self.greenvalue_input.isModified() == False:
-       #     self.greenvalue_input.setText("-1")
+        self.greenvalue_input.setValidator(QIntValidator(0, 255))
 
         self.bluevalue_input.setObjectName("bluevalue_input")
         self.verticalLayout.addWidget(self.bluevalue_input)
-      #  if self.bluevalue_input.isModified() == False:
-      #      self.bluevalue_input.setText("-1")
+        self.bluevalue_input.setValidator(QIntValidator(0, 255))
 
         self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(60, 0, 100, 80))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
@@ -72,7 +75,6 @@ class Ui_MainWindow(object):
         self.BlueValue.setObjectName("BlueValue")
         self.verticalLayout_2.addWidget(self.BlueValue)
 
-
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 254, 18))
         self.menubar.setObjectName("menubar")
@@ -83,11 +85,6 @@ class Ui_MainWindow(object):
 
         self.ConfirmButton.setGeometry(QtCore.QRect(61, 85, 181, 21))
         self.ConfirmButton.setObjectName("ConfirmButton")
-
-        """ self.ConfirmButton.setDisabled(True)
-        if self.bluevalue_input.isModified() == True and self.redvalue_input.isModified() == True and self.greenvalue_input.isModified() == True:
-            self.ConfirmButton.setEnabled(True)
-        """
         self.ConfirmButton.clicked.connect(lambda: self.backend_stuff())
 
         self.retranslateUi(MainWindow)
@@ -101,10 +98,27 @@ class Ui_MainWindow(object):
         self.GreenValue.setText(_translate("MainWindow", "Green Value"))
         self.BlueValue.setText(_translate("MainWindow", "Blue Value"))
 
+    def clearLayout(self, layout):
+        if layout is not None:
+            while layout.count():
+                child = layout.takeAt(0)
+                if child.widget() is not None:
+                    child.widget().deleteLater()
+                elif child.layout() is not None:
+                    self.clearLayout(child.layout())
+        else:
+            pass
+
+    def add_label(self, text):
+        color_text = QtWidgets.QLabel()
+        color_text.setText(text)
+        color_text.setFont(QFont('Arial', 8))
+        color_text.setWordWrap(True)
+        self.clearLayout(self.verticalLayout_Output)
+        self.verticalLayout_Output.addWidget(color_text)
 
     def backend_stuff(self):
         data_read = p.read_csv("colors.csv", delimiter=',', names=['Color names', 'Hex', 'R', 'G', 'B'])
-        # data_red = data_read[['R', 'G', 'B']]
 
         if self.redvalue_input.text() != "":
             R = int(self.redvalue_input.text())
@@ -130,41 +144,25 @@ class Ui_MainWindow(object):
             in_read.columns = ['R', 'G', 'B']
         
             desired_df = p.merge(data_read, in_read, on=['R', 'G', 'B'], how='inner')
-            color_string = p.Series.to_string(desired_df['Color names'])
+            if not desired_df.empty:
+                color_string = p.Series.to_string(desired_df['Color names'])
 
-            print(color_string)
-
+                self.add_label(color_string)
+            else:
+                err_text = "No Color\n Found,\n Try Again"
+                self.add_label(err_text)
         else:
-            print("please insert RGB values to get Color")
+            err_text = "please insert RGB values\n to get Color"
+            self.add_label(err_text)
+
 
 def call_ui():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow(MainWindow)
+    ui = UiMainWindow(MainWindow)
     ui.setupUi(MainWindow)
     ui.input_widgets(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-
-
-"""def backend_stuff(red, green, blue):
-    data_read = p.read_csv("colors.csv", delimiter=',', names=['Color names', 'Hex', 'R', 'G', 'B', ])
-    # data_red = data_read[['R', 'G', 'B']]
-    R = red
-    G = green
-    B = blue
-
-    userdata = [R, G, B]
-    user_df = p.DataFrame(userdata)
-    in_read = p.DataFrame.transpose(p.DataFrame(user_df))
-    in_read.columns = ['R', 'G', 'B']
-
-    in_read['R'] = in_read['R'].astype(int)
-    in_read['G'] = in_read['G'].astype(int)
-    in_read['B'] = in_read['B'].astype(int)
-
-    desired_df = p.merge(data_read, in_read, on=['R', 'G', 'B'], how='inner')
-"""
 
 call_ui()
